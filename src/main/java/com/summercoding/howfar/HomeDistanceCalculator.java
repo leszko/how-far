321d77466c46fc467f421175598a24b7d0a67aaa
Rafal Leszko <rafal.leszko@gmail.com>
2014-06-25 00:10:06 +0200

    Add caching last calculated distance result.

--- src/main/java/com/summercoding/howfar/HomeDistanceCalculator.java
+++ src/main/java/com/summercoding/howfar/HomeDistanceCalculator.java
@@ -10,9 +10,22 @@ public class HomeDistanceCalculator {
     private static final double HOME_LATITUDE = 50.063987;
     private static final double HOME_LONGITUDE = 19.926916;
 
+    private static final double MIN_DIFF = 0.01;
+
+    private double lastLatitude = 0.0;
+    private double lastLongitude = 0.0;
+    private double lastResult = 0.0;
+
     public double distanceInKm(double latitude, double longitude) {
-        float[] result = new float[1];
-        Location.distanceBetween(HOME_LATITUDE, HOME_LONGITUDE, latitude, longitude, result);
-        return result[0] / KILO;
+        if (isDifferenceBigEnough(latitude, longitude)) {
+            float[] result = new float[1];
+            Location.distanceBetween(HOME_LATITUDE, HOME_LONGITUDE, latitude, longitude, result);
+            lastResult = result[0] / KILO;
+        }
+        return lastResult;
+    }
+
+    private boolean isDifferenceBigEnough(double latitude, double longitude) {
+        return Math.abs(lastLatitude - latitude) > MIN_DIFF || Math.abs(lastLongitude - longitude) > MIN_DIFF;
     }
 }
