e082fdece167868c2f0c51d9e7b7c4e52b26a886
Rafal Leszko <rafal.leszko@gmail.com>
2014-06-25 21:17:46 +0200

    Change to Google Play Services.

--- src/main/java/com/summercoding/howfar/MainActivity.java
+++ src/main/java/com/summercoding/howfar/MainActivity.java
@@ -1,21 +1,41 @@
 package com.summercoding.howfar;
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+
 import java.text.DecimalFormat;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends FragmentActivity implements LocationListener,
+        GooglePlayServicesClient.ConnectionCallbacks,
+        GooglePlayServicesClient.OnConnectionFailedListener {
 
     private static final String TAG = "MainActivity";
+
+    private final static int
+            CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+
 
     private HomeDistanceCalculator distanceCalculator = new HomeDistanceCalculator();
 
     private DecimalFormat distanceFormat = new DecimalFormat("#.#");
 
     private TextView mainText;
+    private LocationClient mLocationClient;
+    private LocationRequest mLocationRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +43,89 @@ public class MainActivity extends FragmentActivity {
         setContentView(R.layout.activity_main);
 
         mainText = (TextView) findViewById(R.id.textView);
+        // Create a new global location parameters object
+        mLocationRequest = LocationRequest.create();
+
+        /*
+         * Set the update interval
+         */
+        mLocationRequest.setInterval(LocationUtils.UPDATE_INTERVAL_IN_MILLISECONDS);
+
+        // Use high accuracy
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        // Set the interval ceiling to one minute
+        mLocationRequest.setFastestInterval(LocationUtils.FAST_INTERVAL_CEILING_IN_MILLISECONDS);
+
+        mLocationClient = new LocationClient(this, this, this);
+
+        if (servicesConnected()) {
+            mainText.setText("Services Connected");
+        }
+        else {
+            mainText.setText("Services Not Connected");
+        }
+    }
+
+    /**
+     * Verify that Google Play services is available before making a request.
+     *
+     * @return true if Google Play services is available, otherwise false
+     */
+    private boolean servicesConnected() {
+
+        // Check that Google Play services is available
+        int resultCode =
+                GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+
+        return ConnectionResult.SUCCESS == resultCode;
     }
 
     @Override
-    protected void onStart() {
+    public void onStop() {
+
+        // If the client is connected
+        if (mLocationClient.isConnected()) {
+            stopPeriodicUpdates();
+        }
+
+        // After disconnect() is called, the client is considered "dead".
+        mLocationClient.disconnect();
+
+        super.onStop();
+    }
+
+    /**
+     * In response to a request to start updates, send a request
+     * to Location Services
+     */
+    private void startPeriodicUpdates() {
+
+        mLocationClient.requestLocationUpdates(mLocationRequest, this);
+    }
+
+    /**
+     * In response to a request to stop updates, send a request to
+     * Location Services
+     */
+    private void stopPeriodicUpdates() {
+        mLocationClient.removeLocationUpdates(this);
+    }
+
+    /*
+ * Called when the Activity is restarted, even before it becomes visible.
+ */
+    @Override
+    public void onStart() {
+
         super.onStart();
 
-        MainActivityLocationManager locationManager = new MainActivityLocationManager(this);
-        locationManager.start();
+        /*
+         * Connect the client. Don't re-start any requests here;
+         * instead, wait for onResume()
+         */
+        mLocationClient.connect();
+
     }
 
     public void updateLocation(double latitude, double longitude) {
@@ -39,5 +134,26 @@ public class MainActivity extends FragmentActivity {
         double distance = distanceCalculator.distanceInKm(latitude, longitude);
         String stringDistance = distanceFormat.format(distance);
         mainText.setText(stringDistance + " km");
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        mainText.setText("CONNECTED");
+
+    }
+
+    @Override
+    public void onDisconnected() {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
     }
 }
