a788d1e199918d6a872e700fdc8f291e188e43d0
Rafal Leszko <rafal.leszko@gmail.com>
2014-07-02 17:40:49 +0200

    Remove caching from HomeDistanceCalculator.

--- src/main/java/com/summercoding/howfar/HomeDistanceCalculator.java
+++ src/main/java/com/summercoding/howfar/HomeDistanceCalculator.java
@@ -5,39 +5,18 @@ import android.location.Location;
 public class HomeDistanceCalculator {
     private static final double KILO = 1000;
 
-    private static final double MIN_DIFF = 0.001;
-
     private double homeLatitude = 0.0;
     private double homeLongitude = 0.0;
-
-    private double lastLatitude = 0.0;
-    private double lastLongitude = 0.0;
-    private double lastResult = 0.0;
 
     public void setHome(Location location) {
         Preconditions.checkNotNull(location);
         homeLatitude = location.getLatitude();
         homeLongitude = location.getLongitude();
-        lastLatitude = 0.0;
-        lastLongitude = 0.0;
     }
 
     public double distanceInKm(double latitude, double longitude) {
-        if (isDifferenceBigEnough(latitude, longitude)) {
-            lastLatitude = latitude;
-            lastLongitude = longitude;
-            lastResult = calculateDistanceInKm(latitude, longitude);
-        }
-        return lastResult;
-    }
-
-    private double calculateDistanceInKm(double latitude, double longitude) {
         float[] result = new float[1];
         Location.distanceBetween(homeLatitude, homeLongitude, latitude, longitude, result);
         return result[0] / KILO;
-    }
-
-    private boolean isDifferenceBigEnough(double latitude, double longitude) {
-        return Math.abs(lastLatitude - latitude) > MIN_DIFF || Math.abs(lastLongitude - longitude) > MIN_DIFF;
     }
 }
