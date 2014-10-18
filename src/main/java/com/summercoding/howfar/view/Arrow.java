500e19369c7c5b6eaece68f441faab0a8ef97597
Rafal Leszko <rafal.leszko@gmail.com>
2014-10-18 21:21:48 +0200

    Change local variables to fields in Arrow.

--- src/main/java/com/summercoding/howfar/view/Arrow.java
+++ src/main/java/com/summercoding/howfar/view/Arrow.java
@@ -11,10 +11,31 @@ import android.view.View;
 
 public class Arrow extends View {
 
+    private final Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+    private final Point pointA = new Point();
+    private final Point pointB = new Point();
+    private final Point pointC = new Point();
+    private final Point pointD = new Point();
+
+    private int paddingLeft;
+    private int paddingTop;
+    private int paddingRight;
+    private int paddingBottom;
+
     private float rotation = 0f;
 
     public Arrow(Context context, AttributeSet attrs) {
         super(context, attrs);
+
+        initPaint();
+    }
+
+    private void initPaint() {
+        paint.setColor(Color.WHITE);
+        paint.setStyle(Paint.Style.FILL_AND_STROKE);
+        paint.setAntiAlias(true);
+        paint.setStrokeWidth(2);
     }
 
     public void setRotation(float rotation) {
@@ -23,43 +44,39 @@ public class Arrow extends View {
     }
 
     @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+
+        paddingLeft = getPaddingLeft();
+        paddingTop = getPaddingTop();
+        paddingRight = getPaddingRight();
+        paddingBottom = getPaddingBottom();
+    }
+
+    @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        // TODO: consider storing these as member variables to reduce
-        // allocations per draw cycle.
-        int paddingLeft = getPaddingLeft();
-        int paddingTop = getPaddingTop();
-        int paddingRight = getPaddingRight();
-        int paddingBottom = getPaddingBottom();
-
         int contentWidth = getWidth() - paddingLeft - paddingRight;
         int contentHeight = getHeight() - paddingTop - paddingBottom;
-
-        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-
-        paint.setColor(Color.WHITE);
-        paint.setStyle(Paint.Style.FILL_AND_STROKE);
-        paint.setAntiAlias(true);
-        paint.setStrokeWidth(2);
 
         int size = Math.min(contentHeight, contentWidth);
         int arrowLength = size - size / 4;
         int arrowWidth = size / 6;
         int holeSize = size / 5;
 
-        Point a = new Point(contentWidth / 2, contentHeight / 2 - arrowLength / 2);
-        Point b = new Point(contentWidth / 2 - arrowWidth, contentHeight / 2 + arrowLength / 2);
-        Point c = new Point(contentWidth / 2, contentHeight / 2 + arrowLength / 2 - holeSize);
-        Point d = new Point(contentWidth / 2 + arrowWidth, contentHeight / 2 + arrowLength / 2);
+        pointA.set(contentWidth / 2, contentHeight / 2 - arrowLength / 2);
+        pointB.set(contentWidth / 2 - arrowWidth, contentHeight / 2 + arrowLength / 2);
+        pointC.set(contentWidth / 2, contentHeight / 2 + arrowLength / 2 - holeSize);
+        pointD.set(contentWidth / 2 + arrowWidth, contentHeight / 2 + arrowLength / 2);
 
         Path path = new Path();
         path.setFillType(Path.FillType.EVEN_ODD);
-        path.moveTo(a.x, a.y);
-        path.lineTo(b.x, b.y);
-        path.lineTo(c.x, c.y);
-        path.lineTo(d.x, d.y);
-        path.lineTo(a.x, a.y);
+        path.moveTo(pointA.x, pointA.y);
+        path.lineTo(pointB.x, pointB.y);
+        path.lineTo(pointC.x, pointC.y);
+        path.lineTo(pointD.x, pointD.y);
+        path.lineTo(pointA.x, pointA.y);
 
         canvas.rotate(rotation, contentWidth / 2, contentHeight / 2);
         canvas.drawPath(path, paint);
